f337db0d40a044aff36fe689f30e2899463c571e
swsoe <npt1@zips.uakron.edu>
2014-05-30 15:50:47 -0400

    Fixed Scripts/FallingPlatform.cs  Platform had no gravity but could be moved.  The attribute isKinematic will disable/enable a rigid body from moving and interacting with the environment.  Script will take a float and delay the platform's fall in seconds.

--- runner/Assets/Scripts/FallingPlatform.cs
+++ runner/Assets/Scripts/FallingPlatform.cs
@@ -1,15 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class FallingPlatform : MonoBehaviour {
+public class FallingPlatform : MonoBehaviour 
+{
 
-	// Use this for initialization
-	void Start () {
-		transform.rigidbody2D.gravityScale = 0;
+    public float delay = 5.0f;
+    
+    // Use this for initialization
+	void Start () 
+    {
+        transform.rigidbody2D.isKinematic = true;
 	}
 
-	void OnCollisionEnter2D(Collision2D collision) {
-		if (collision.gameObject.tag == "Player")
-			transform.rigidbody2D.gravityScale = 5f;
+	void OnCollisionEnter2D(Collision2D collision) 
+    {
+        if (collision.gameObject.tag == "Player")
+            Invoke("Fall", delay);
 	}
+
+    void Fall()
+    {
+        transform.rigidbody2D.isKinematic = false;
+    }
 }
