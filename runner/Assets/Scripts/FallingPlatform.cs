5ae9328f77214dea43af0c86974503396b8b2393
Drew Guarnera <dtg3@zips.uakron.edu>
2014-05-30 12:35:00 -0400

    Sandbox!

---
+++ runner/Assets/Scripts/FallingPlatform.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallingPlatform : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		transform.rigidbody2D.gravityScale = 0;
+	}
+
+	void OnCollisionEnter2D() {
+		transform.rigidbody2D.gravityScale = 5f;
+	}
+}
