7a10f81061828c9686ee62f00c2efef5bf75e7ff
Drew Guarnera <dtg3@zips.uakron.edu>
2014-05-31 21:26:08 -0400

    Add support for wobbling trap platform

--- runner/Assets/Scripts/FallingPlatform.cs
+++ runner/Assets/Scripts/FallingPlatform.cs
@@ -4,11 +4,31 @@ using System.Collections;
 public class FallingPlatform : MonoBehaviour {
 
 	bool trapActive = false;
+	bool back = false;
+	Vector3 startPos;
+
 	// Use this for initialization
 	void Start () {
 		trapActive = false;
+		back = false;
+		startPos = transform.position;
 	}
 
+	void Update () {
+		if (trapActive) {
+			if (!back) {
+				transform.Translate(Vector3.right * Time.deltaTime * 4f);
+				if (transform.position.x > startPos.x + .2f)
+					back = true;
+			}
+			else {
+				transform.Translate(Vector3.left * Time.deltaTime * 4f);
+				if (transform.position.x < startPos.x - .2f)
+					back = false;
+			}
+		}
+	}
+	
 	void OnCollisionEnter2D(Collision2D collision) {
 		if (collision.gameObject.tag == "Player")
 			if (!trapActive) {
@@ -18,8 +38,9 @@ public class FallingPlatform : MonoBehaviour {
 	}
 
 	public IEnumerator Fall() {
-		yield return new WaitForSeconds(1f);
-		Rigidbody2D trap = this.gameObject.AddComponent<Rigidbody2D> ();
+		yield return new WaitForSeconds(.4f);
+		trapActive = false;
+		Rigidbody2D trap = this.gameObject.AddComponent<Rigidbody2D> (); //This introduces a runtime warning, but hasn't proven game breaking. Just ignore for now.
 		trap.gravityScale = 5f;
 	}
 }
