eef0477e8bd51c22ee0282e4b441ac1d8a8c5646
Drew Guarnera <dtg3@zips.uakron.edu>
2014-06-04 12:46:02 -0400

    Add Title Screen

---
+++ runner/Assets/Scripts/ClickAnimator.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickAnimator : MonoBehaviour {
+
+	Animator anim;
+	bool over = false;
+
+	void Start() {
+		anim = GetComponent<Animator>();
+	}
+	void Update() {
+		anim.SetBool("Over", over);
+	}
+
+	void OnMouseEnter ()
+	{
+		over = true;
+	}
+	
+	void OnMouseExit () 
+	{
+		over = false;
+	}
+
+	void OnMouseDown () {
+		//Application.LoadLevel ("");
+	}
+}
