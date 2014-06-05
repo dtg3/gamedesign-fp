9f14d54c5fa8e8a8b1b8d1cbac15f810d1c8f425
Drew Guarnera <dtg3@zips.uakron.edu>
2014-06-05 10:34:57 -0400

    SPAWNER!!!

---
+++ runner/Assets/Scripts/PlayerSpawner.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSpawner : MonoBehaviour {
+
+	public GameObject[] characters;
+
+	// Use this for initialization
+	void Start () {
+		Instantiate (characters[CharSel.selectedCharacter]);
+	}
+}
