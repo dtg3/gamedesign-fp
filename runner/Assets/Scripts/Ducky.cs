cf51d96782326242cc482bb608029c117ce588bc
Drew Guarnera <dtg3@zips.uakron.edu>
2014-06-05 12:19:51 -0400

    Stub for duck squeak

--- runner/Assets/Scripts/Ducky.cs
+++ runner/Assets/Scripts/Ducky.cs
@@ -3,13 +3,9 @@ using System.Collections;
 
 public class Ducky : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	void OnCollisionEnter2D(Collision2D collision) {
+		if (collision.gameObject.tag == "Player") {
+			//audio.PlayOneShot();
+		}
 	}
 }
