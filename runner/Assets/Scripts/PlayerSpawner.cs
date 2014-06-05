05ab3fd0e3944cc25f5219bdf986a524cf24a0b4
Drew Guarnera <dtg3@zips.uakron.edu>
2014-06-05 11:04:52 -0400

    Spawn fixes

--- runner/Assets/Scripts/PlayerSpawner.cs
+++ runner/Assets/Scripts/PlayerSpawner.cs
@@ -6,7 +6,8 @@ public class PlayerSpawner : MonoBehaviour {
 	public GameObject[] characters;
 
 	// Use this for initialization
-	void Start () {
-		Instantiate (characters[CharSel.selectedCharacter]);
+
+	void Awake () {
+		Instantiate (characters[CharSel.selectedCharacter], new Vector3(transform.position.x, transform.position.y, -1f), Quaternion.identity);	
 	}
 }
